a2d534596cba52c40f80fac60ec27272e8772505
dwy1991 <1581685256@qq.com>
2020-05-20 08:41:06 +0800

    Update and rename pjp.c to first.c
    第一个c语言程序，

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()           /*一个简单的c程序*/
+{
+  int num;          /*定义一个num的变量*/
+  num=1;            //为num赋值
+  
+  printf("I am a simple ");                //使用printf函数
+  printf("computer.\n");
+  printf("My favorite number is %d because it is first .\n", num);
+  return 0;
+}
+    
