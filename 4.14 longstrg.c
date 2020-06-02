df3e17aa21bda75e74672b898ec0ef0ea2afdd5d
dwy1991 <1581685256@qq.com>
2020-06-02 12:09:38 +0800

    Create 4.14 longstrg.c

---
+++ 4.14 longstrg.c
@@ -0,0 +1,11 @@
+//longstrg.c --打印较长的字符串
+#include<stdio.h>
+int main()
+{
+	printf("Here's one way to print a ");
+	printf("long string.\n");
+	printf("Here's another way to print a \
+long stting.\n");   //适合输出较长文本
+    printf("Here's the newest way to print a ""long stting.\n") ;
+	return 0;                      
+}
